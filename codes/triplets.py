d6006b6245ddb85c1063c916870b8b7e968c51f8
cinghioz <mdossena20@gmail.com>
2025-08-20 13:04:11 +0000

    initial commit

---
+++ codes/triplets.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python3
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pandas as pd
+import numpy as np
+import torch
+import os
+
+class TripletsEngine:
+    def __init__(self, path, from_splits=False, feat_path=None):
+        self.path = path
+        self.from_splits = from_splits
+        self.feat_path = feat_path
+        self.triplets = np.array([], dtype=object)
+        self.entity_to_id = {}
+        self.relation_to_id = {}
+        self.number_of_entities = 0
+        self.number_of_relations = 0
+        if from_splits:
+            self._load_triplets_from_splits(
+            os.path.join(path, "train.txt"),
+            os.path.join(path, "valid.txt"),
+            os.path.join(path, "test.txt")
+        )
+        elif os.path.isdir(self.path) and os.path.isfile(self.path+'/entities.txt') and os.path.isfile(self.path+'/relations.txt'):
+                self._load_triplets_proc()
+        else:
+                self._load_triplets()
+        if feat_path:
+            self._load_features()
+    
+    def _load_triplets_proc(self):
+        df = pd.read_csv(self.path+'/triplets.txt', sep='\t', header=None)
+        self.triplets = df.values
+        self.number_of_relations = len(self.triplets)
+
+        ents = pd.read_csv(self.path+'/entities.txt', sep='\t', header=None, names=['id', 'name'])
+        self.entity_to_id = ents.set_index('name')['id'].to_dict()
+        self.number_of_entities = len(self.entity_to_id)
+
+        rels = pd.read_csv(self.path+'/relations.txt', sep='\t', header=None, names=['id', 'name'])
+        self.relation_to_id = rels.set_index('name')['id'].to_dict()
+
+    def _load_triplets_from_splits(self, train_file, valid_file, test_file):
+        # Load files
+        train_df = pd.read_csv(train_file, sep='\t', header=None)
+        valid_df = pd.read_csv(valid_file, sep='\t', header=None)
+        test_df  = pd.read_csv(test_file, sep='\t', header=None)
+
+        all_df = pd.concat([train_df, valid_df, test_df], ignore_index=True)
+        self.triplets = all_df.values
+
+        self._index_triplets()
+
+        train_size = len(train_df)
+        valid_size = len(valid_df)
+
+        self.train_set = np.arange(0, train_size)
+        self.valid_set = np.arange(train_size, train_size + valid_size)
+        self.test_set  = np.arange(train_size + valid_size, len(self.triplets))
+
+    def _load_triplets(self):
+        if self.path.endswith('.csv'):
+            self._load_triplets_csv()
+        elif self.path.endswith('.txt'):
+            self._load_triplets_txt()
+        else:
+            raise ValueError("Unsupported file format. Use .csv or .txt")
+        
+        self._index_triplets()
+        
+    def _load_triplets_csv(self):
+        df = pd.read_csv(self.path)
+        self.triplets = df.values
+
+    def _load_triplets_txt(self):
+        df = pd.read_csv(self.path, sep='\t')
+        self.triplets = df.values
+
+    def _index_triplets(self):
+        unique_entities = np.unique(np.concatenate((self.triplets[:, 0], self.triplets[:, 2])))
+        unique_relations = np.unique(self.triplets[:, 1])
+
+        self.number_of_entities = len(unique_entities)
+        self.number_of_relations = len(unique_relations)
+
+        self.entity_to_id = {entity: i for i, entity in enumerate(unique_entities)}
+        self.relation_to_id = {relation: i for i, relation in enumerate(unique_relations)}
+
+        # Look up the indices for heads, relations, and tails in their respective sorted arrays
+        head_ids = np.searchsorted(unique_entities, self.triplets[:, 0])
+        relation_ids = np.searchsorted(unique_relations, self.triplets[:, 1])
+        tail_ids = np.searchsorted(unique_entities, self.triplets[:, 2])
+
+        self.triplets = np.column_stack((head_ids, relation_ids, tail_ids))
+
+    def _load_features(self):
+        self.node_features = torch.load(self.feat_path)
+
+    def split_triplets(self, train_ratio=0.7, valid_ratio=0.10, inductive=False, seed=42):
+        np.random.seed(seed)
+        triplets_to_split = self.triplets
+        self.inductive_set = np.array([]) 
+
+        if inductive:
+            unique_entities = np.array(list(self.entity_to_id.values()))
+            entity_count = len(unique_entities)
+            inductive_entity_count = max(1, int(entity_count * 0.02)) # Ensure at least 1, take 2% of entities
+
+            inductive_entities = np.random.choice(unique_entities, size=inductive_entity_count, replace=False)
+
+            head_is_inductive = np.isin(self.triplets[:, 0], inductive_entities)
+            tail_is_inductive = np.isin(self.triplets[:, 2], inductive_entities)
+            
+            # Use logical OR to get a mask for any triplet containing an inductive entity.
+            inductive_mask = np.logical_or(head_is_inductive, tail_is_inductive)
+
+            self.inductive_set = self.triplets[inductive_mask]
+            triplets_to_split = self.triplets[~inductive_mask]
+
+            print(f"Held out {len(inductive_entities)} entities.")
+            print(f"Moved {len(self.inductive_set)} triplets to the inductive set.")
+            print(f"{len(triplets_to_split)} triplets remain for train/valid/test.")
+            print("-" * 20)
+
+        total_triplets = len(triplets_to_split)
+        indices = np.random.permutation(total_triplets)
+
+        train_size = int(total_triplets * train_ratio)
+        valid_size = int(total_triplets * valid_ratio)
+
+        self.train_set = indices[:train_size]
+        self.valid_set = indices[train_size:train_size + valid_size]
+        self.test_set = indices[train_size + valid_size:]
+
+        del triplets_to_split
+
+        return len(self.train_set), len(self.valid_set), len(self.test_set)
+    
+    def generate_negative_samples(self, n, positive_batch = np.array([]), part_to_corrupt='head'):
+        existing_set = {tuple(triplet) for triplet in self.triplets}
+        
+        if positive_batch.size > 0:
+            base_triplets = np.array(positive_batch)
+            n = len(base_triplets)
+        else:
+            source_indices = np.random.randint(0, len(self.triplets), size=n)
+            base_triplets = self.triplets[source_indices]
+
+        random_replacements = np.random.randint(0, self.number_of_entities, size=n)
+        corrupted_triplets = base_triplets.copy()
+        
+        if part_to_corrupt == 'head':
+            col_idx = 0
+        elif part_to_corrupt == 'tail':
+            col_idx = 2
+        else:
+            raise ValueError("part_to_corrupt must be 'head' or 'tail'.")
+
+        for i in range(n):
+            # Initial corruption with the pre-generated random replacement.
+            corrupted_triplets[i, col_idx] = random_replacements[i]
+            
+            # This inner loop handles collisions: it re-samples only if the corruption is invalid.
+            # This happens rarely, so it doesn't slow down the process significantly.
+            while (tuple(corrupted_triplets[i]) in existing_set or 
+                corrupted_triplets[i, col_idx] == base_triplets[i, col_idx]):
+                
+                # Generate a new random replacement and try again.
+                corrupted_triplets[i, col_idx] = np.random.randint(0, self.number_of_entities)
+
+        return corrupted_triplets
---
+++ codes/utils.py
@@ -0,0 +1,141 @@
+import argparse
+import json
+import logging
+import os
+import random
+
+import numpy as np
+import torch
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='Training and Testing Knowledge Graph Embedding Models',
+        usage='train.py [<args>] [-h | --help]'
+    )
+
+    parser.add_argument('--cuda', action='store_true', help='use GPU')
+    
+    parser.add_argument('--do_train', action='store_true')
+    parser.add_argument('--do_valid', action='store_true')
+    parser.add_argument('--do_test', action='store_true')
+    parser.add_argument('--evaluate_train', action='store_true', help='Evaluate on training data')
+    
+    parser.add_argument('--data_path', type=str, default=None)
+    parser.add_argument('--model', default='TransE', type=str)
+    parser.add_argument('-de', '--double_entity_embedding', action='store_true')
+    parser.add_argument('-dr', '--double_relation_embedding', action='store_true')
+    
+    parser.add_argument('-n', '--negative_sample_size', default=128, type=int)
+    parser.add_argument('-d', '--hidden_dim', default=500, type=int)
+    parser.add_argument('-g', '--gamma', default=12.0, type=float)
+    parser.add_argument('-adv', '--negative_adversarial_sampling', action='store_true')
+    parser.add_argument('-a', '--adversarial_temperature', default=1.0, type=float)
+    parser.add_argument('-b', '--batch_size', default=1024, type=int)
+    parser.add_argument('-r', '--regularization', default=0.0, type=float)
+    parser.add_argument('--test_batch_size', default=4, type=int, help='valid/test batch size')
+    parser.add_argument('--uni_weight', action='store_true', 
+                        help='Otherwise use subsampling weighting like in word2vec')
+    
+    parser.add_argument('-lr', '--learning_rate', default=0.0001, type=float)
+    parser.add_argument('-cpu', '--cpu_num', default=32, type=int)
+    parser.add_argument('-init', '--init_checkpoint', default=None, type=str)
+    parser.add_argument('-save', '--save_path', default=None, type=str)
+    parser.add_argument('--max_steps', default=100000, type=int)
+    parser.add_argument('--warm_up_steps', default=None, type=int)
+    
+    parser.add_argument('--save_checkpoint_steps', default=1000, type=int)
+    parser.add_argument('--valid_steps', default=5000, type=int)
+    parser.add_argument('--log_steps', default=100, type=int, help='train log every xx steps')
+    parser.add_argument('--test_log_steps', default=1000, type=int, help='valid/test log every xx steps')
+    
+    parser.add_argument('--nentity', type=int, default=0, help='DO NOT MANUALLY SET')
+    parser.add_argument('--nrelation', type=int, default=0, help='DO NOT MANUALLY SET')
+    
+    return parser.parse_args(args)
+
+def override_config(args):
+    '''
+    Override model and data configuration
+    '''
+    
+    with open(os.path.join(args.init_checkpoint, 'config.json'), 'r') as fjson:
+        argparse_dict = json.load(fjson)
+    
+    if args.data_path is None:
+        args.data_path = argparse_dict['data_path']
+
+    args.model = argparse_dict['model']
+    args.double_entity_embedding = argparse_dict['double_entity_embedding']
+    args.double_relation_embedding = argparse_dict['double_relation_embedding']
+    args.hidden_dim = argparse_dict['hidden_dim']
+    args.test_batch_size = argparse_dict['test_batch_size']
+    
+def save_model(model, optimizer, save_variable_list, args):
+    '''
+    Save the parameters of the model and the optimizer,
+    as well as some other variables such as step and learning_rate
+    '''
+    
+    argparse_dict = vars(args)
+    with open(os.path.join(args.save_path, 'config.json'), 'w') as fjson:
+        json.dump(argparse_dict, fjson)
+
+    torch.save({
+        **save_variable_list,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict()},
+        os.path.join(args.save_path, 'checkpoint')
+    )
+    
+    entity_embedding = model.entity_embedding.detach().cpu().numpy()
+    np.save(
+        os.path.join(args.save_path, 'entity_embedding'), 
+        entity_embedding
+    )
+    
+    relation_embedding = model.relation_embedding.detach().cpu().numpy()
+    np.save(
+        os.path.join(args.save_path, 'relation_embedding'), 
+        relation_embedding
+    )
+
+def set_logger(args):
+    '''
+    Write logs to checkpoint and console
+    '''
+
+    if args.do_train:
+        log_file = os.path.join(args.save_path or args.init_checkpoint, 'train.log')
+    else:
+        log_file = os.path.join(args.save_path or args.init_checkpoint, 'test.log')
+
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename=log_file,
+        filemode='w'
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+def log_metrics(mode, step, metrics):
+    '''
+    Print the evaluation logs
+    '''
+    for metric in metrics:
+        logging.info('%s %s at step %d: %f' % (mode, metric, step, metrics[metric]))
+
+# def read_triple(file_path, entity2id, relation2id):
+#     '''
+#     Read triples and map them into ids.
+#     '''
+#     triples = []
+#     with open(file_path) as fin:
+#         for line in fin:
+#             h, r, t = line.strip().split('\t')
+#             triples.append((entity2id[h], relation2id[r], entity2id[t]))
+#     return triples
